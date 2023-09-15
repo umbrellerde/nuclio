70f7f5e2b60dbc52727e01c4eb0599f2d610fceb
Trever Schirmer <trever@umbreller.de>
2023-09-15 11:48:09 +0200

    add stats

--- profaastinate/evaluation/terraform/main.tf
+++ profaastinate/evaluation/terraform/main.tf
@@ -36,10 +36,11 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-12"
+      size = 16
     }
   }
 
-  metadata_startup_script = "sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y git ca-certificates curl gnupg make && curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh"
+  metadata_startup_script = "sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git ca-certificates curl gnupg make && curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh"
 
   //metadata_startup_script = file("../deployment_scripts/installDeps.sh")
 
