a81ac2ba5d5d3d9fc6080e53b7f115dfb21f0188
Trever Schirmer <trever@umbreller.de>
2023-09-15 09:26:31 +0200

    🗺 improve terraform

--- profaastinate/evaluation/terraform/main.tf
+++ profaastinate/evaluation/terraform/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "google" {
   project = "schirmer-project"
-  region = "europe-west3"
-  zone = "europe-west3-a"
+  region = "europe-west4"
+  zone = "europe-west4-a"
 
   credentials = file(var.key_file_location)
 }
@@ -24,12 +24,13 @@ resource "google_compute_subnetwork" "default" {
   ip_cidr_range = "10.0.1.0/24"
   name          = "profaastinate-subnetwork"
   network       = google_compute_network.vpc_network.id
+  region = "europe-west4"
 }
 
 resource "google_compute_instance" "default" {
-  machine_type = "c3-standard-4" # "e2-standard-2"
+  machine_type = "c3-standard-8" # "e2-standard-2"
   name         = "default-vm"
-  zone = "europe-west3-a"
+  zone = "europe-west4-a"
   tags = ["ssh"]
 
   boot_disk {
@@ -37,6 +38,8 @@ resource "google_compute_instance" "default" {
       image = "debian-cloud/debian-12"
     }
   }
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get upgrade && sudo apt-get install -y git ca-certificates curl gnupg make && curl -fsSL get.docker.com -o get-docker.sh && sudo sh get-docker.sh"
 
   //metadata_startup_script = file("../deployment_scripts/installDeps.sh")
 
@@ -52,7 +55,7 @@ resource "google_compute_instance" "default" {
 }
 
 resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
+  name = "allow-ssh-profaastinate"
   allow {
     protocol = "tcp"
     ports = ["22"]
--- profaastinate/evaluation/terraform/output.tf
+++ profaastinate/evaluation/terraform/output.tf
@@ -1,3 +1,13 @@
 output "test_server_ip" {
   value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
+
+output "test_server_ssh_login" {
+  value = "ssh debian@${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  description = "run this command to log into the server"
+}
+
+output "test_server_ssh_forward" {
+  value = "ssh -N -L 9000:localhost:9000 -L 8070:localhost:8070 debian@${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+  description = "run this command to be able to access the gui of nuclio and minio from localhost (i.e., you can go to localhost:9000 to open the remotely running nuclio gui)"
+}
